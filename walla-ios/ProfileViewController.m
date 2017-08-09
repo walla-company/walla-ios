08afb8a2408ac11228a16fb97f0b1e793cdf1456
Stas Tomych <stas.tomich@gmail.com>
2017-08-09 12:29:09 +0300

    Profle UI implemented

--- walla-ios/ProfileViewController.m
+++ walla-ios/ProfileViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "ProfileViewController.h"
+#import "WAServer.h"
+
+@import Firebase;
 
 @interface ProfileViewController () <UITableViewDelegate, UITableViewDataSource>
 
@@ -16,12 +19,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
+    self.tableView.estimatedRowHeight = 100; // Something reasonable to help ios render your cells
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    
+    [super viewWillAppear:animated];
+    
+    [WAServer getUserWithID:[FIRAuth auth].currentUser.uid completion:^(WAUser *user) {
+        
+        self.user = user;
+        
+       // [self.tableView reloadData];
+        
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+;
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return UITableViewAutomaticDimension;
 }
 
 @end
